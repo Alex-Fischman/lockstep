e0bd66487a6d3f3526caab5965926d6d599c9d9f
Alex <alex@fischman.org>
2022-01-15 14:34:15 -0800

    Two circles raymarched

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,30 @@ use winit::{
 	window::Window,
 };
 
+#[repr(C)]
+struct Uniforms {
+	aspect_ratio: f32,
+}
+
+impl Uniforms {
+	fn new() -> Uniforms {
+		Uniforms { aspect_ratio: 1.0 }
+	}
+
+	fn update(&mut self, window: &winit::window::Window) {
+		self.aspect_ratio = window.inner_size().height as f32 / window.inner_size().width as f32;
+	}
+
+	fn as_bytes(&self) -> &[u8] {
+		unsafe {
+			std::slice::from_raw_parts(
+				(self as *const Uniforms) as *const u8,
+				std::mem::size_of::<Uniforms>(),
+			)
+		}
+	}
+}
+
 async fn run(event_loop: EventLoop<()>, window: Window) {
 	let size = window.inner_size();
 	let instance = wgpu::Instance::new(wgpu::Backends::all());
@@ -35,10 +59,39 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 		source: wgpu::ShaderSource::Wgsl(include_str!("shader.wgsl").into()),
 	});
 
+	let bind_group_layout = device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
+		label: None,
+		entries: &[wgpu::BindGroupLayoutEntry {
+			binding: 0,
+			visibility: wgpu::ShaderStages::FRAGMENT,
+			ty: wgpu::BindingType::Buffer {
+				ty: wgpu::BufferBindingType::Uniform,
+				has_dynamic_offset: false,
+				min_binding_size: None,
+			},
+			count: None,
+		}],
+	});
 	let pipeline_layout = device.create_pipeline_layout(&wgpu::PipelineLayoutDescriptor {
 		label: None,
-		bind_group_layouts: &[],
+		bind_group_layouts: &[&bind_group_layout],
 		push_constant_ranges: &[],
+	});
+
+	let mut uniforms = Uniforms::new();
+	let uniform_buffer = device.create_buffer(&wgpu::BufferDescriptor {
+		label: Some("Uniform Buffer"),
+		size: uniforms.as_bytes().len() as u64,
+		usage: wgpu::BufferUsages::UNIFORM | wgpu::BufferUsages::COPY_DST,
+		mapped_at_creation: false,
+	});
+	let bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
+		layout: &bind_group_layout,
+		entries: &[wgpu::BindGroupEntry {
+			binding: 0,
+			resource: uniform_buffer.as_entire_binding(),
+		}],
+		label: None,
 	});
 
 	let swapchain_format = surface.get_preferred_format(&adapter).unwrap();
@@ -76,6 +129,8 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 				config.width = size.width;
 				config.height = size.height;
 				surface.configure(&device, &config);
+				uniforms.update(&window);
+				queue.write_buffer(&uniform_buffer, 0, uniforms.as_bytes());
 			}
 			Event::RedrawRequested(_) => {
 				let frame = surface
@@ -85,7 +140,7 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 				let mut encoder = device
 					.create_command_encoder(&wgpu::CommandEncoderDescriptor { label: None });
 				{
-					let mut rpass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+					let mut pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
 						label: None,
 						color_attachments: &[wgpu::RenderPassColorAttachment {
 							view: &view,
@@ -97,8 +152,9 @@ async fn run(event_loop: EventLoop<()>, window: Window) {
 						}],
 						depth_stencil_attachment: None,
 					});
-					rpass.set_pipeline(&render_pipeline);
-					rpass.draw(0..6, 0..2);
+					pass.set_pipeline(&render_pipeline);
+					pass.set_bind_group(0, &bind_group, &[]);
+					pass.draw(0..6, 0..2);
 				}
 
 				queue.submit(Some(encoder.finish()));
--- src/shader.wgsl
+++ src/shader.wgsl
@@ -1,17 +1,62 @@
+struct VertexOutput {
+    [[builtin(position)]] clip: vec4<f32>;
+    [[location(0)]]       frag: vec2<f32>;
+};
+
 [[stage(vertex)]]
-fn vert_main([[builtin(vertex_index)]] i: u32) -> [[builtin(position)]] vec4<f32> {
+fn vert_main([[builtin(vertex_index)]] i: u32) -> VertexOutput {
+    var pos = vec4<f32>(0.0, 0.0, 0.0, 1.0);
     switch (i) {
-        case 0:  { return vec4<f32>(-1.0,  1.0, 0.0, 1.0); }
-        case 1:  { return vec4<f32>(-1.0, -1.0, 0.0, 1.0); }
-        case 2:  { return vec4<f32>( 1.0,  1.0, 0.0, 1.0); }
-        case 3:  { return vec4<f32>(-1.0, -1.0, 0.0, 1.0); }
-        case 4:  { return vec4<f32>( 1.0,  1.0, 0.0, 1.0); }
-        case 5:  { return vec4<f32>( 1.0, -1.0, 0.0, 1.0); }
-        default: { return vec4<f32>( 0.0,  0.0, 0.0, 0.0); }
+        case 0:  { pos = vec4<f32>(-1.0,  1.0, 0.0, 1.0); }
+        case 1:  { pos = vec4<f32>(-1.0, -1.0, 0.0, 1.0); }
+        case 2:  { pos = vec4<f32>( 1.0,  1.0, 0.0, 1.0); }
+        case 3:  { pos = vec4<f32>(-1.0, -1.0, 0.0, 1.0); }
+        case 4:  { pos = vec4<f32>( 1.0,  1.0, 0.0, 1.0); }
+        case 5:  { pos = vec4<f32>( 1.0, -1.0, 0.0, 1.0); }
+        default: { pos = vec4<f32>( 0.0,  0.0, 0.0, 0.0); }
     }
+    return VertexOutput(pos, pos.xy);
 }
 
+fn sphere(p: vec3<f32>) -> f32 {
+    return length(p) - 1.0;
+}
+
+fn sdf(p: vec3<f32>) -> f32 {
+    return min(sphere(p), sphere(p - vec3<f32>(2.0, 0.0, 0.0)));
+}
+
+struct Hit {
+    pos: vec3<f32>;
+    iter: u32;
+    dist: f32;
+};
+
+let MIN_DIST = 0.01;
+let MAX_DIST = 1000.0;
+let MAX_ITER = 10u;
+fn raymarch(eye: vec3<f32>, dir: vec3<f32>) -> Hit {
+    var out = Hit(eye, 0u, 0.0);
+    loop {
+        let dist = sdf(out.pos);
+        if (abs(dist) < MIN_DIST || abs(dist) > MAX_DIST || out.iter > MAX_ITER) { break; }
+        out.iter = out.iter + 1u;
+        out.dist = min(out.dist, dist);
+        out.pos  = out.pos  + normalize(dir) * dist;
+    }
+    return out;
+}
+
+struct Uniforms {
+    aspect_ratio: f32;
+};
+[[group(0), binding(0)]]
+var<uniform> uniforms: Uniforms;
+
 [[stage(fragment)]]
-fn frag_main() -> [[location(0)]] vec4<f32> {
-    return vec4<f32>(1.0, 0.0, 0.0, 1.0);
+fn frag_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
+    let eye = vec3<f32>(in.frag.x, in.frag.y * uniforms.aspect_ratio, -5.0);
+    let hit = raymarch(eye * 5.0, vec3<f32>(0.0, 0.0, 1.0));
+    if (abs(sdf(hit.pos)) < MIN_DIST) { return vec4<f32>(1.0); }
+    else { return vec4<f32>(0.0, 0.0, 0.0, 1.0); }
 }
