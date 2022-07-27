1afcc71a7321591f016c7f8edbe0aa27138e1be6
Alex <alex@fischman.org>
2022-07-26 20:20:38 -0700

    Added extrude, is slow, removed render in loop

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ const PIXELS: usize = (WIDTH * HEIGHT) as usize;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
 	let sdl = sdl2::init()?;
+	let mut pump = sdl.event_pump()?;
 	let video = sdl.video()?;
 	let window = video.window("lockstep", WIDTH, WIDTH).position_centered().build()?;
 	let mut canvas = window.into_canvas().build()?;
@@ -16,9 +17,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 		HEIGHT,
 	)?;
 	let mut pixels = [Color::BLACK; PIXELS];
-	let mut then = std::time::Instant::now();
-	let mut state = State::new();
-	let mut pump = sdl.event_pump()?;
+
+	let then = std::time::Instant::now();
+	render(&mut pixels);
+	let now = std::time::Instant::now();
+	println!("{:?}", now - then);
+
+	texture.update(
+		None,
+		unsafe { std::slice::from_raw_parts(pixels.as_ptr() as *const u8, PIXELS) },
+		std::mem::size_of::<Color>() * WIDTH as usize,
+	)?;
+	canvas.copy(&texture, None, None)?;
+	canvas.present();
+
 	loop {
 		for event in pump.poll_iter() {
 			match event {
@@ -26,21 +38,6 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 				_ => {}
 			}
 		}
-
-		update(&mut state);
-		render(&state, &mut pixels);
-		texture.update(
-			None,
-			unsafe { std::slice::from_raw_parts(pixels.as_ptr() as *const u8, PIXELS) },
-			std::mem::size_of::<Color>() * WIDTH as usize,
-		)?;
-		canvas.copy(&texture, None, None)?;
-		canvas.present();
-
-		let now = std::time::Instant::now();
-		let delta = now - then;
-		then = now;
-		println!("{:?}", delta);
 	}
 }
 
@@ -58,6 +55,13 @@ impl std::ops::Sub for Vector {
 	type Output = Vector;
 	fn sub(self, other: Vector) -> Vector {
 		Vector(self.0 - other.0, self.1 - other.1, self.2 - other.2)
+	}
+}
+
+impl std::ops::Neg for Vector {
+	type Output = Vector;
+	fn neg(self) -> Vector {
+		Vector(-self.0, -self.1, -self.2)
 	}
 }
 
@@ -96,69 +100,68 @@ impl Ray {
 	}
 }
 
-struct SDF(Box<dyn Fn(Vector) -> f64>);
-
-impl std::fmt::Debug for SDF {
-	fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
-		write!(f, "SDF")
-	}
-}
-
-impl SDF {
-	fn sphere() -> SDF {
-		SDF(Box::new(move |v: Vector| v.length() - 1.0))
-	}
-
-	fn union(a: SDF, b: SDF) -> SDF {
-		SDF(Box::new(move |v: Vector| f64::min(a.0(v), b.0(v))))
-	}
-
-	fn translate(self, t: Vector) -> SDF {
-		SDF(Box::new(move |v: Vector| self.0(v - t)))
-	}
-
-	fn scale(self, s: f64) -> SDF {
-		SDF(Box::new(move |v: Vector| self.0(1.0 / s * v) * s))
-	}
-}
-
-const DISTANCE_MIN: f64 = 0.01;
-const DISTANCE_MAX: f64 = 10.0;
-const ITERATION_MAX: u64 = 100;
-fn raymarch(sdf: &SDF, ray: Ray) -> Option<(f64, u64)> {
-	let mut distance = 0.0;
-	let mut iteration = 0;
-	while distance < DISTANCE_MAX && iteration < ITERATION_MAX {
-		match sdf.0(ray.pos + distance * ray.dir) {
-			d if d < DISTANCE_MIN => return Some((distance, iteration)),
-			d => distance += d,
-		}
-		iteration += 1;
-	}
-	None
-}
-
 #[derive(Debug)]
-struct State {
-	scene: SDF,
+enum SDF<'a> {
+	Sphere(f64),
+	Union(&'a SDF<'a>, &'a SDF<'a>),
+	Translate(&'a SDF<'a>, Vector),
+	Extrude(&'a SDF<'a>, Vector),
 }
 
-impl State {
-	fn new() -> State {
-		State {
-			scene: SDF::union(
-				SDF::sphere().translate(Vector(0.5, 0.0, 2.0)),
-				SDF::sphere().scale(0.75).translate(Vector(-1.0, 0.0, 3.0)),
-			),
+impl<'a> SDF<'a> {
+	const ITERATION_MAX: u64 = 100;
+	fn run(&self, v: Vector) -> f64 {
+		match self {
+			SDF::Sphere(r) => v.length() - r,
+			SDF::Union(a, b) => f64::min(a.run(v), b.run(v)),
+			SDF::Translate(a, u) => a.run(v - *u),
+			SDF::Extrude(a, u) => {
+				let ray = Ray::new(v, -*u);
+				let mut min_dist = f64::MAX;
+				let mut distance = 0.0;
+				let mut iteration = 0;
+				while distance < u.length() && iteration < SDF::ITERATION_MAX {
+					let d = a.run(ray.pos + distance * ray.dir);
+					min_dist = min_dist.min(d);
+					distance += d.abs();
+					iteration += 1;
+				}
+				min_dist
+			}
 		}
 	}
-}
 
-fn update(_state: &mut State) {}
+	fn union(&'a self, other: &'a SDF) -> SDF<'a> {
+		SDF::Union(self, other)
+	}
+
+	fn translate(&'a self, t: Vector) -> SDF<'a> {
+		SDF::Translate(self, t)
+	}
+
+	const DISTANCE_MIN: f64 = 0.01;
+	const DISTANCE_MAX: f64 = 10.0;
+	fn raymarch(&self, ray: Ray) -> Option<(f64, u64)> {
+		let mut distance = 0.0;
+		let mut iteration = 0;
+		while distance < SDF::DISTANCE_MAX && iteration < SDF::ITERATION_MAX {
+			match self.run(ray.pos + distance * ray.dir) {
+				d if d < SDF::DISTANCE_MIN => return Some((distance, iteration)),
+				d => distance += d,
+			}
+			iteration += 1;
+		}
+		None
+	}
+}
 
 use std::f64::consts::PI;
 const FOV: f64 = PI / 2.0;
-fn render(state: &State, pixels: &mut [Color; PIXELS]) {
+fn render(pixels: &mut [Color; PIXELS]) {
+	let a = SDF::Sphere(1.00).translate(Vector(0.5, 0.0, 2.0));
+	let b = SDF::Sphere(0.75).translate(Vector(-1.0, 0.0, 3.0));
+	let c = SDF::union(&a, &b);
+	let scene = SDF::Extrude(&c, -Vector::Y);
 	for x in 0..WIDTH {
 		for y in 0..HEIGHT {
 			let i = (x + y * HEIGHT) as usize;
@@ -166,7 +169,7 @@ fn render(state: &State, pixels: &mut [Color; PIXELS]) {
 			let y = y as f64 / HEIGHT as f64 - 0.5;
 			let z = (PI * 0.5 - FOV * 0.5).tan() * 0.5;
 			let ray = Ray::new(Vector::ZERO, Vector(x, y, z).normalized());
-			pixels[i] = match raymarch(&state.scene, ray) {
+			pixels[i] = match scene.raymarch(ray) {
 				None => Color::BLACK,
 				Some((_dist, _iter)) => Color::WHITE,
 			}
